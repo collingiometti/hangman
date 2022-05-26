3d2956ab64f9f611b27c7ff5f75d0fb1b43dc3fe
duxyrk <64509670+duxyrk@users.noreply.github.com>
2022-05-25 20:27:49 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -13,22 +13,23 @@ height = 600
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 
-def button(word,x,y,w,h,c1,c2,action=None):
-  mouse_pos = pygame.mouse.get_pos()
+def button(word,x,y,w,h,ic,ac,action=None):
+  mouse = pygame.mouse.get_pos()
   click = pygame.mouse.get_pressed()
   
-  if x+w > mouse_pos[0] > x and y+h > mouse_pos[1] > y:
-    pygame.draw.rect(screen,c2,(x,y,w,h))
+  print(mouse)
+  if x+w > mouse[0] > x and y+h > mouse[1] > y:
+    pygame.draw.rect(screen,ac,(x,y,w,h))
     if click[0] == 1 and action != None:
       action()
-    else:
-      pygame.draw.rect(screen,c1,(x,y,w,h))
-      
-  text = pygame.font.SysFont('Corbel',20)
-  text_surface = text.render(word, True, black)
-  text_rect = text_surface.get_rect()
-  text_rect.center = ((x+(w/2)),(y+(h/2)))
-  screen.blit(text_surface, text_rect)
+  else:
+    pygame.draw.rect(screen,ic,(x,y,w,h))
+
+  buttonText = pygame.font.SysFont("Corbel",40)
+  buttonTextSurf = buttonText.render(word, True, black)
+  buttonTextRect = buttonTextSurf.get_rect()
+  buttonTextRect.center = ((x+(w/2)), (y+(h/2)))
+  screen.blit(buttonTextSurf, buttonTextRect)
 
 def gamechoice():
   print("Hello World")
@@ -39,6 +40,6 @@ def hangman():
 pygame.init()
 while True:
   screen.fill(background)
-  button('test',100,100,200,50,button_light,button_dark,hangman)
-  pygame.display.flip()
+  button('test',100,100,200,50,button_light,black,hangman)
+  pygame.display.update()
   clock.tick(30)
